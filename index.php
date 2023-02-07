53cc68bb8fbdb88b6c87e34752df5c5d15468447
Rani Derradj <raniderradj2@gmail.com>
2023-02-07 16:52:59 +0100

    ajout de la fonctionnalité add vapoteuse

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
@@ -32,10 +33,10 @@ $selectTable = ("SELECT * FROM Vapfactory.Vapoteuses");
 // et on l'affiche avec la méthode query
 $query = $db->query($selectTable);
 
-$title = $query->fetchAll(PDO::FETCH_OBJ);
-echo '<pre>';
-print_r($title);
-echo '</pre>';
+$title = $query->fetchAll(PDO::FETCH_ASSOC);
+// echo '<pre>';
+// print_r($title);
+// echo '</pre>';
 
 
 
@@ -44,21 +45,44 @@ echo '</pre>';
 
 }
 ?>
- <?php
- $titles = [
-    "Nom de l'article" -> $name,
-    "Description de l'article"-> $desciption,
-    "Référence" ->$reference
 
- ]
-
-?>
+<?php foreach($title as $titles):?>
 <ul>
-    <?php foreach($title as $titles):?>
-    <li><?= $titles[0] ?></li>
-    <?php endforeach ?>
-
+    <?= $titles["Nom de l'article"] ?>
+<img src="images/pod.jpg" alt="">
+    <ol class="liste description">Description : <?= $titles["Description de l'article"] ?></ol>
+    <ol class="liste">Référence :<?= $titles["Référence"] ?></ol>
+    <ol class="liste">Prix de Vente Unitaire : <?= $titles["Prix de vente unitaire"] ?></ol>
+    <ol class="liste">Prix d'achat unitaire : <?= $titles["Prix d'achat unitaire"] ?></ol>
+    <ol class="liste">Quantité en stock : <?= $titles["Quantité en stock"] ?></ol>
+    <input type="button" value="Modifier la fiche produit">
 </ul>
+<?php endforeach ?>
 
+<form action="">
+    <h2> Ajouter une nouvelle Vapoteuse </h2>
+    <div class="main_form">
+
+<label for="name">Nom de la vapoteuse</label>
+<input type="text" for="name">
+
+<label for="name">Description</label>
+<input type="text" for="name">
+
+<label for="name">Référence </label>
+<input type="text" for="name">
+
+<label for="name">Prix de Vente Unitaire : </label>
+<input type="text" for="name">
+
+<label for="name">Prix d'achat unitaire</label>
+<input type="text" for="name">
+
+<label for="name">Quantité </label>
+<input type="text" for="name">
+
+<input type="submit">
+</div>
+</form>
 </body>
 </html>
